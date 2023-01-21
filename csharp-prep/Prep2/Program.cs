1916b70dc178d83bc9a2db2c2d41c21076c923dc
Henrykinyua <kar21022@byui.edu>
2023-01-21 14:32:08 +0300

    prep two assignment completed.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,12 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("what's your first name? ");
-        string first = Console.ReadLine();
+        Console.Write("what's your grade percentage? ");
+        string userGrade = Console.ReadLine();
+        int percent = int.Parse(userGrade);
         
-        Console.WriteLine("What's your last name");
-        string last = Console.ReadLine();
+        string grade = "";
 
-        Console.WriteLine($"{first}, {last} {first}");
+        if (percent>= 90)
+        {
+            grade =  "A";
+        }
+
+        else if (percent >= 80)
+        {
+            grade =  "B";
+        } 
+
+        else if (percent >= 70)
+        {
+            grade =  "C";  
+        }
+
+        else if (percent >= 60)
+        {
+            grade =  "D";
+        } 
+        else 
+        {
+            grade =  "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulation!, You passed the class");
+        } 
+        else if (percent < 70)
+        {
+            Console.WriteLine("Sorry you failed, there is still next time, please try harder.");
+        }
     }
-}
+} 
