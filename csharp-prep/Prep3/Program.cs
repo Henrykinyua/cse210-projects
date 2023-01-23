30fb0f645379b0ab3ec8cbea1bc2457dc812331e
Henrykinyua <kar21022@byui.edu>
2023-01-23 13:19:29 +0300

    completed Prep3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+        Console.WriteLine($"Magic number is {magicNumber}");
+
+        int guess = -1;
+        while (guess != magicNumber)
+        {   
+            Console.Write("What is your guess? ");
+            string userNumber = Console.ReadLine();
+            guess = int.Parse(userNumber);                                      
+            
+            if (guess <  magicNumber)
+            {
+                Console.WriteLine("Higher");   
+            }
+            
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it right!");
+            }
+        }    
     }
 }
